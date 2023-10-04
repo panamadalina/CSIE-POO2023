4f7d563567bf02564557b0cd90e88f0747ded075
Madalina Pana <44752259+panamadalina@users.noreply.github.com>
2023-10-04 12:34:50 +0300

    Add files via upload

---
+++ seminar2-exemplu1.cpp
@@ -0,0 +1,28 @@
+/*de rezolvat erorile*/
+
+#include <iostream>
+using namespace std;
+
+int Main()
+{
+    int x = 10;
+    int y = 15;
+
+    cout << " " << (x, y)
+
+        while (!)
+        {
+            cout << "hello";
+        }
+
+    int n = 9, div = 0;
+    div = n / 0;   cout << "result = " << div;
+
+    for (int i = 0; i < 3; i++);
+    {
+        cout << "loop ";
+        continue;
+
+    }
+
+}
